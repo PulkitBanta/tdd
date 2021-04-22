444539076f8df408eaf39e78a6937c53d8ac3a23
Pulkit <Pulkitbanta55555@gmail.com>
2021-04-22 16:08:16 +0530

    vowel and consonant lower case

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -2,7 +2,20 @@ package co.interleap.courses.tdd;
 
 public class VowelCounter {
 
+    public boolean isVowel(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        return false;
+    }
+
     public int count(String inp) {
-        return inp.length();
+        int vowelFound = 0;
+
+        for (char ch : inp.toCharArray()) {
+            if (isVowel(ch))
+                vowelFound++;
+        }
+
+        return vowelFound;
     }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -7,17 +7,22 @@ import static org.junit.Assert.assertEquals;
 public class VowelCounterTest {
 
     @Test
-    public void emptyStringTest() {
+    public void emptyString() {
         assertEquals(0, new VowelCounter().count(""));
     }
 
     @Test
-    public void singleVowelTest() {
+    public void singleVowel() {
         assertEquals(1, new VowelCounter().count("a"));
     }
 
     @Test
-    public void multipleVowelTest() {
+    public void multipleVowels() {
         assertEquals(5, new VowelCounter().count("aeiou"));
+    }
+
+    @Test
+    public void vowelAndConsonantLowerCase() {
+        assertEquals(2, new VowelCounter().count("hello"));
     }
 }
