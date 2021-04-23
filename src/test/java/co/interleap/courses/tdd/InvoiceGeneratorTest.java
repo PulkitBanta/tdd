e3c47c74e45f76e3a60250c6f6721f90099ebde3
Pulkit <Pulkitbanta55555@gmail.com>
2021-04-23 11:14:59 +0530

    multiple ride test

--- src/test/java/co/interleap/courses/tdd/InvoiceGeneratorTest.java
+++ src/test/java/co/interleap/courses/tdd/InvoiceGeneratorTest.java
@@ -2,6 +2,8 @@ package co.interleap.courses.tdd;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class InvoiceGeneratorTest {
@@ -14,6 +16,15 @@ public class InvoiceGeneratorTest {
     @Test
     public void shouldCalculateFareForFiveKmAndTwentyMin() {
         assertEquals(70, new InvoiceGenerator().fare(5, 20));
+    }
+
+    @Test
+    public void shouldCalculateFareForMultipleRides() {
+        ArrayList<Ride> rides = new ArrayList<Ride>();
+        rides.add(new Ride(4, 10));
+        rides.add(new Ride(5, 20));
+
+        assertEquals(120, new InvoiceGenerator().fare(rides));
     }
 
 }
